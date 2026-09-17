b76c1841c3216ab8bbed386fb1209e6c4d19fee9
niedoborpotasu <edziennikjj@gmail.com>
2026-09-17 10:32:24 +0200

    create new sqlite database

---
+++ backend/Migrations/20260917081732_InitialCreate.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", nullable: true),
+                    IsMale = table.Column<bool>(type: "INTEGER", nullable: true),
+                    PlaceOfBirth = table.Column<string>(type: "TEXT", nullable: true),
+                    DateOfBirth = table.Column<DateOnly>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Directors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: false),
+                    Bio = table.Column<string>(type: "TEXT", nullable: true),
+                    IsMale = table.Column<bool>(type: "INTEGER", nullable: true),
+                    PlaceOfBirth = table.Column<string>(type: "TEXT", nullable: true),
+                    DateOfBirth = table.Column<DateOnly>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false),
+                    IsAdmin = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MediaItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Poster = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<int>(type: "INTEGER", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    DirectorId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Rating = table.Column<double>(type: "REAL", nullable: true),
+                    MediaType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Genres = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MediaItems_Directors_DirectorId",
+                        column: x => x.DirectorId,
+                        principalTable: "Directors",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActorMediaItem",
+                columns: table => new
+                {
+                    ActorsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MediaItemsId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActorMediaItem", x => new { x.ActorsId, x.MediaItemsId });
+                    table.ForeignKey(
+                        name: "FK_ActorMediaItem_Actors_ActorsId",
+                        column: x => x.ActorsId,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActorMediaItem_MediaItems_MediaItemsId",
+                        column: x => x.MediaItemsId,
+                        principalTable: "MediaItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seasons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SeasonNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    MediaItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: true),
+                    PremiereDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    EndDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seasons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seasons_MediaItems_MediaItemId",
+                        column: x => x.MediaItemId,
+                        principalTable: "MediaItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMediaItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    MediaItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: true),
+                    Review = table.Column<string>(type: "TEXT", nullable: true),
+                    WatchedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    WatchStatus = table.Column<int>(type: "INTEGER", nullable: true),
+                    IsFavorite = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMediaItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserMediaItems_MediaItems_MediaItemId",
+                        column: x => x.MediaItemId,
+                        principalTable: "MediaItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMediaItems_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Episodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EpisodeNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    SeasonId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    PremiereDate = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    Rating = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Episodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Episodes_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSeasons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SeasonId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: true),
+                    Review = table.Column<string>(type: "TEXT", nullable: true),
+                    WatchedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    WatchStatus = table.Column<int>(type: "INTEGER", nullable: true),
+                    IsFavorite = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSeasons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserSeasons_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserSeasons_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEpisodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EpisodeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: true),
+                    Review = table.Column<string>(type: "TEXT", nullable: true),
+                    WatchedAt = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    WatchStatus = table.Column<int>(type: "INTEGER", nullable: true),
+                    IsFavorite = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEpisodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserEpisodes_Episodes_EpisodeId",
+                        column: x => x.EpisodeId,
+                        principalTable: "Episodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserEpisodes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActorMediaItem_MediaItemsId",
+                table: "ActorMediaItem",
+                column: "MediaItemsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_SeasonId",
+                table: "Episodes",
+                column: "SeasonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaItems_DirectorId",
+                table: "MediaItems",
+                column: "DirectorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_MediaItemId",
+                table: "Seasons",
+                column: "MediaItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserEpisodes_EpisodeId",
+                table: "UserEpisodes",
+                column: "EpisodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserEpisodes_UserId",
+                table: "UserEpisodes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMediaItems_MediaItemId",
+                table: "UserMediaItems",
+                column: "MediaItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMediaItems_UserId",
+                table: "UserMediaItems",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSeasons_SeasonId",
+                table: "UserSeasons",
+                column: "SeasonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSeasons_UserId",
+                table: "UserSeasons",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActorMediaItem");
+
+            migrationBuilder.DropTable(
+                name: "UserEpisodes");
+
+            migrationBuilder.DropTable(
+                name: "UserMediaItems");
+
+            migrationBuilder.DropTable(
+                name: "UserSeasons");
+
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "Episodes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Seasons");
+
+            migrationBuilder.DropTable(
+                name: "MediaItems");
+
+            migrationBuilder.DropTable(
+                name: "Directors");
+        }
+    }
+}
